debdb2fc005a7186febdd8fbb90d47b07bf12629
pretotn <nicolas.pretot@epitech.eu>
2014-04-26 16:22:30 +0200

    adding Install-autologin Test

---
+++ Installer/update-tmp-file/Install-Autologin.sh
@@ -0,0 +1,11 @@
+# Creating autologin script
+echo "
+#!/bin/bash
+exec login -f dev
+" > autologin
+echo dev | sudo -S mv autologin /sbin/autologin
+echo dev | sudo -S chmod 755 /sbin/autologin
+
+#Loading autologin script
+cat /etc/init/tty1.conf | sed -e "s/exec \/sbin\/getty \-8 38400 tty1/exec \/sbin\/getty \-n \-l \/sbin\/autologin 38400 tty1/" > tmp.cfg
+echo dev | sudo -S mv tmp.cfg /etc/init/tty1.conf
