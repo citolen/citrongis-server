be4af6f011114d1eb86697415668d2bb3f82468a
pretotn <nicolas.pretot@epitech.eu>
2014-05-05 19:34:41 +0200

    Add Install http-server

---
+++ Installer/update-file/Install-HttpServer.sh
@@ -0,0 +1,5 @@
+echo dev | sudo -S apt-get -y install apache2
+echo dev | sudo -S apt-get -y install php5
+echo dev | sudo -S apt-get -y install libapache2-mod-php5
+echo dev | sudo -S apt-get -y install mysql-server
+echo dev | sudo -S apt-get -y install php5-mysql
