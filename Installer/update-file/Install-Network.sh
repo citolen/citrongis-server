c074c343a6810efd0612c1767cc821412cfcd474
pretotn <nicolas.pretot@epitech.eu>
2014-05-05 19:17:41 +0200

    Install network

---
+++ Installer/update-file/Install-Network.sh
@@ -0,0 +1,14 @@
+echo "
+auto lo
+iface lo inet loopback
+
+auto eth1
+iface eth1 inet static
+address 192.168.56.101
+netmask 255.255.255.0
+network 192.168.56.0
+broadcast 192.168.56.255
+" >> interfaces
+echo dev | sudo -S rm /etc/network/interfaces
+echo dev | sudo -S mv interfaces /etc/network/interfaces
+echo dev | sudo -S chmod 644 /etc/network/interfaces 
