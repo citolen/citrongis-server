cb442be9e6986675742c40e2d1497503429e0776
pretotn <nicolas.pretot@epitech.eu>
2014-06-04 19:46:48 +0200

    Fix couchDb Instalation

--- Installer/update-file/Install-CouchDB.sh
+++ Installer/update-file/Install-CouchDB.sh
@@ -2,3 +2,6 @@ echo "dev" | sudo -S apt-get install couchdb -y
 echo "dev" | sudo -S touch /etc/couchdb/local.ini
 echo "dev" | sudo -S mkdir /etc/couchdb/local.d
 echo "dev" | sudo -S mkdir /var/run/couchdb
+
+cat /etc/couchdb/default.ini | sed -e "s/port \= 5984/port \= 5985/" > tmp.cfg
+echo "dev" | sudo -S mv tmp.cfg /etc/couchdb/default.ini
