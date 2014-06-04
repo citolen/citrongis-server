26760265250af37e7a0cdc24cbf44f698fbf2088
pretotn <nicolas.pretot@epitech.eu>
2014-06-04 19:30:03 +0200

    Fix couchDb Instalation Again

--- Installer/update-file/Install-CouchDB.sh
+++ Installer/update-file/Install-CouchDB.sh
@@ -1,1 +1,4 @@
-echo "dev" | sudo -S apt-get install couchdb-bin -y
+echo "dev" | sudo -S apt-get install couchdb -y
+echo "dev" | sudo -S touch /etc/couchdb/local.ini
+echo "dev" | sudo -S mkdir /etc/couchdb/local.d
+echo "dev" | sudo -S mkdir /var/run/couchdb
