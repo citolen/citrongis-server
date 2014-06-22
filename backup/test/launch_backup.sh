f7e835235c817635e1642c1ba2d269d0beffd206
pretotn <nicolas.pretot@epitech.eu>
2014-06-22 21:00:54 +0200

    Adding full Export/Import script + Test Tree

---
+++ backup/test/launch_backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+#Note : This Script is used to launch the backup process.
+#	Parameters can be given in the command line, or default value will be used.
+
+cmd="export";
+zip_path=`readlink -f .`;
+input_mongodb="origin/mongodb";
+input_couchdb="origin/couchdb";
+input_file="origin/file";
+
+if [ $# -ge 1 ]
+then
+    cmd=$1
+    if [ $# -ge 2 ]
+    then
+	zip_path=`readlink -f $2`;
+	if [ $# -ge 3 ]
+	then
+	    input_mongodb=`readlink -f $3`;
+	    if [ $# -ge 4 ]
+	    then
+		input_couchdb=`readlink -f $4`;
+		if [ $# -ge 5 ]
+		then
+		    input_file=`readlink -f $5`;
+		fi
+	    fi
+	fi
+    fi
+fi
+
+if [ $cmd == "import" ]
+then
+    ./backup.sh $cmd $zip_path $input_mongodb $input_couchdb $input_file;
+elif [ $cmd == "export" ]
+then
+    ./backup.sh $cmd $zip_path $input_mongodb $input_couchdb $input_file;
+fi
