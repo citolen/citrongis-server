9f49a607aab20d266049815def4757323c20a161
pretotn <nicolas.pretot@epitech.eu>
2014-09-08 17:09:49 +0200

    update

--- source/user_api/router/AuthRouter/authRouter.js
+++ source/user_api/router/AuthRouter/authRouter.js
@@ -13,40 +13,48 @@ function authRouter(app) {
 authRouter.prototype.login = function(app) {
     app.route("/auth/login")
 	.all(function (req, res, next) {
-	    console.log("ici");    
+	    console.log("Route : auth/login");    
+	    next();
 	})
 	.get(function (req, res, next) {
-	    console.log("ici");    
+	    answerGet(res);
 	})
 	.post(function (req, res, next) {
-	    console.log("ici");    
+	    console.log("POST");
 	})
 }
 
 authRouter.prototype.logout = function(app) {
-    app.route("/auth/login")
+    app.route("/auth/logout")
 	.all(function (req, res, next) {
-	    
+	    console.log("Route : auth/logout");    
+	    next();
 	})
 	.get(function (req, res, next) {
-
+	    answerGet(res);
 	})
 	.post(function (req, res, next) {
-
+	    console.log("POST");
 	})
 }
 
 authRouter.prototype.subscribe = function(app) {
-    app.route("/auth/login")
+    app.route("/auth/subscribe")
 	.all(function (req, res, next) {
-	    
+	    console.log("Route : auth/subscribe");    
+	    next();
 	})
 	.get(function (req, res, next) {
-
+	    answerGet(res);
 	})
 	.post(function (req, res, next) {
-
+	    console.log("POST");
 	})
+}
+
+function answerGet(res) {
+    console.log("GET");
+    res.send("GET : OK");
 }
 
 module.exports = authRouter;
