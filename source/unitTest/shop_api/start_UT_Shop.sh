a168f7e3f6bbd5e0a6d7e3f511e32a8b1df60cd6
gabila-r <romain.gabilan@epitech.eu>
2014-12-14 14:44:06 -0500

    clean unit tests

---
+++ source/unitTest/shop_api/start_UT_Shop.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "   - Launch shop_api/UT_testAuth.js"
+nodejs shop_api/UT_testAuth.js
+
+echo "   - Clear Database ..."
+./db_cleaner.sh "localhost:27017" > /dev/null
+
+echo "   - Launch shop_api/UT_upload.js"
+nodejs shop_api/UT_upload.js
+
+echo "   - Clear Database ..."
+./db_cleaner.sh "localhost:27017" > /dev/null
+
+echo "   - Launch shop_api/UT_upload.js"
+nodejs shop_api/UT_uploadBadFiles.js
