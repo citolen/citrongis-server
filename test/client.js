8dc25f96274dc604ed25583b1ae90ee3874b1eec
gabila_r <romain.gabilan@epitech.eu>
2014-12-17 21:45:27 -0500

    test fonctionnel

--- test/client.js
+++ test/client.js
@@ -1,56 +1,49 @@
 var request = require('request');
 var fs = require('fs');
 
-function download_txt() {
+function download_txt_get() {
 
-    var download_form = {};
-    
-    var post_form = {
-	url: 'http://localhost:6060/download_txt',
-	form: download_form
-    };
-    
-    request.post(post_form, function(err, res, body) {
-	var ws = fs.createWriteStream("./output/test_out.txt");
-	res.pipe(ws);
-
-	res.on("end", function() {
-	    console.log("event end");
-	})
-	res.on("data", function(chunk) {
-	    console.log("event data");
-	});
-	ws.on('error', function (err) {
-	    console.log("err : " + err)
-	});
-    });
+request
+  .get('http://localhost:6060/download_txt')
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .pipe(fs.createWriteStream('./output/test_out_get.txt'))
 }
 
-function download_zip() {
+function download_zip_get() {
 
-    var download_form = {};
-    
-    var post_form = {
-	url: 'http://localhost:6060/download_zip',
-	form: download_form
-    };
-    
-    request.post(post_form, function(err, res, body) {
-	var ws = fs.createWriteStream("./output/test_out.zip");
-	res.pipe(ws);
-
-	res.on("end", function() {
-	    console.log("event end");
-	})
-	res.on("data", function(chunk) {
-	    console.log("event data");
-	});
-	ws.on('error', function (err) {
-	    console.log("err : " + err)
-	});
-    });
+request
+  .get('http://localhost:6060/download_zip')
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .pipe(fs.createWriteStream('./output/test_out_get.zip'))
 }
 
 
-download_txt();
-download_zip();
+function download_txt_post() {
+
+request
+  .post('http://localhost:6060/download_txt')
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .pipe(fs.createWriteStream('./output/test_out_post.txt'))
+}
+
+function download_zip_post() {
+
+request
+  .post('http://localhost:6060/download_zip')
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .pipe(fs.createWriteStream('./output/test_out_post.zip'))
+}
+
+
+download_txt_get();
+download_zip_get();
+download_txt_post();
+download_zip_post();
