db9633da33d6382aed9279c245352eeb15c97656
gabila_r <romain.gabilan@epitech.eu>
2015-01-14 16:55:50 -0500

    fix one bug

--- source/unitTest/assert.js
+++ source/unitTest/assert.js
@@ -7,13 +7,18 @@ function assert(value, attempt, bool) {
 		try {
 			var obj = JSON.parse(value);
 			var test = JSON.parse(attempt);
-			
+		
+
 			if (Object.keys(obj).length == Object.keys(test).length) {
 				for (var key in obj) {
 					if (!(test.hasOwnProperty(key))) {
 					    console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
 				    	return (counter++);
 				  	}
+					if (!(test[key] == obj[key])) {
+						console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
+				    	return (counter++);
+					}
 				}
 			    console.log(counter + " => " + "Ok".green);
 			}
@@ -21,10 +26,11 @@ function assert(value, attempt, bool) {
 			    console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
 		}
 		catch (e) {
+			console.log(e);
 			if (((value == attempt) == bool)) 
-			    console.log(counter + " => " + "Ok".green);
+			    console.log(counter + " =>1 " + "Ok".green);
 			else
-			    console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
+			    console.log(counter + " =>2 " + "Ko ( ".red + value.red + " )".red);
 		}
 		counter++;
     }
@@ -34,4 +40,20 @@ function assert(value, attempt, bool) {
 
 module.exports = assert;
 
-//assert(JSON.stringify({'romain': 0, 'toto': 1}), JSON.stringify({'toto': 1, 'romain': 0}), true)
+			var s1 = [
+				{"userInfo_firstName": "UserName1"},
+				{"userInfo_firstName": "UserName2"}
+			];
+
+			var s2 = [
+				{"userInfo_firstName": "UserName1"},
+				{"userInfo_firstName": "UserName2"}
+			];
+
+
+assert(JSON.stringify({'romain': 1, 'toto': 'ok'}),
+		JSON.stringify({'toto': 'ok', 'romain': 1}), true)
+
+
+/*assert(JSON.stringify(s1),
+		JSON.stringify(s2), true)*/
