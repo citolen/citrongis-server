7e7f62938e4a36fcd69572a5d1ca73780f264b83
gabila_r <romain.gabilan@epitech.eu>
2015-01-15 01:03:23 -0500

    fix assert

--- source/unitTest/assert.js
+++ source/unitTest/assert.js
@@ -15,9 +15,15 @@ function assert(value, attempt, bool) {
 					    console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
 				    	return (counter++);
 				  	}
-					if (!(test[key] == obj[key])) {
-						console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
-				    	return (counter++);
+
+			  		if (typeof(test[key]) === "object") {
+				  		return (assert(JSON.strngify(obj[key]), JSON.stringify(test[key]), bool));
+					}
+					else { 
+						if (!(test[key] == obj[key])) {
+							console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
+				    		return (counter++);
+						}
 					}
 				}
 			    console.log(counter + " => " + "Ok".green);
@@ -26,34 +32,15 @@ function assert(value, attempt, bool) {
 			    console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
 		}
 		catch (e) {
-			console.log(e);
 			if (((value == attempt) == bool)) 
-			    console.log(counter + " =>1 " + "Ok".green);
+			    console.log(counter + " => " + "Ok".green);
 			else
-			    console.log(counter + " =>2 " + "Ko ( ".red + value.red + " )".red);
+			    console.log(counter + " => " + "Ko ( ".red + value.red + " )".red);
 		}
 		counter++;
     }
     else
-	console.log(counter + " => " + "Ko ( null value )".red);
+		console.log(counter + " => " + "Ko ( null value )".red);
 }
 
 module.exports = assert;
-
-			var s1 = [
-				{"userInfo_firstName": "UserName1"},
-				{"userInfo_firstName": "UserName2"}
-			];
-
-			var s2 = [
-				{"userInfo_firstName": "UserName1"},
-				{"userInfo_firstName": "UserName2"}
-			];
-
-
-assert(JSON.stringify({'romain': 1, 'toto': 'ok'}),
-		JSON.stringify({'toto': 'ok', 'romain': 1}), true)
-
-
-/*assert(JSON.stringify(s1),
-		JSON.stringify(s2), true)*/
