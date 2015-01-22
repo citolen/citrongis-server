42f6da9eeeb9405e4d989f92c1f19a2b37152c50
gabila_r <romain.gabilan@epitech.eu>
2015-01-22 14:33:21 -0500

    add UT_download in general launch

--- source/unitTest/shop_api/start_UT_Shop.sh
+++ source/unitTest/shop_api/start_UT_Shop.sh
@@ -2,25 +2,25 @@
 
 echo "   - Clear Database ..."
 ./db_cleaner.sh "localhost:27017" > /dev/null
-
 echo "   - Launch shop_api/UT_testAuth.js"
 nodejs shop_api/UT_testAuth.js
 
 echo "   - Clear Database ..."
 ./db_cleaner.sh "localhost:27017" > /dev/null
-
 echo "   - Launch shop_api/UT_upload.js"
 nodejs shop_api/UT_upload.js
 
 echo "   - Clear Database ..."
 ./db_cleaner.sh "localhost:27017" > /dev/null
-
 echo "   - Launch shop_api/UT_uploadBadFiles.js"
 nodejs shop_api/UT_uploadBadFiles.js
 
 echo "   - Clear Database ..."
 ./db_cleaner.sh "localhost:27017" > /dev/null
-
 echo "   - Launch shop_api/UT_getExt.js"
 nodejs shop_api/UT_getExt.js
 
+echo "   - Clear Database ..."
+./db_cleaner.sh "localhost:27017" > /dev/null
+echo "   - Launch shop_api/UT_download.js"
+nodejs shop_api/UT_download.js
