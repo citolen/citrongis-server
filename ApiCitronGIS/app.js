f78a395777461ebab9514318fbd77297e48c2ff6
vanwesm <mat-mathieu@orange.fr>
2015-09-05 15:18:46 +0200

    enable cross-origin resource sharing (CORS)

--- ApiCitronGIS/app.js
+++ ApiCitronGIS/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.all(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
