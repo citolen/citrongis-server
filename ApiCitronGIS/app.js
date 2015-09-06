225d6d6fa5f5a4edbb4e7e7a193c4ef7f3ab1cd5
Romain Gabilan <romain.gabilan@epitech.eu>
2015-09-06 18:44:47 +0200

    Update app.js

--- ApiCitronGIS/app.js
+++ ApiCitronGIS/app.js
@@ -25,6 +25,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "*");
     next();
 });
 
